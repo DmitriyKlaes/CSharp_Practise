84d971efc5dc026a119172863a1485ef34228285
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-12 22:14:08 +0300

    initial commit

---
+++ Task006_Random/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программму, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+78 -> 8
+15 -> 5
+92 -< 9
+*/
+
+int randomNumber = new Random().Next(10,100);
+
+int numberOne = randomNumber % 10;
+int numberTwo = randomNumber / 10;
+System.Console.Write($"У числа {randomNumber} большая цифра ");
+if (numberOne > numberTwo)
+{
+    System.Console.Write(numberOne);
+}
+else
+{
+    System.Console.Write(numberTwo);
+}
---
+++ Task007_DeleteCenterNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int randomNumber = new Random().Next(100,1000);
+System.Console.WriteLine(randomNumber);
+int number1 = (randomNumber / 100) * 10;
+int number2 = randomNumber % 10;
+int result = number1 + number2;
+System.Console.WriteLine(result);
