ffc48967c8b1acd069d47202af25c62670119c9d
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-18 22:39:34 +0300

    add tasks 011/012/013/014 from seminar 22-05-16

---
+++ task011_1-QuaterXYvoid/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Напишите программу, которая принимает на вход координаты точки (X и Y), причем X != 0 и Y != 0 и выдает номер четверти плоскости, 
+в которой нахрдится эта точка.
+*/
+
+int GetNumberOfQuater(int X, int Y)
+{
+    int result = 0;
+    if (X > 0 && Y > 0)
+    {
+        result = 1;
+    }
+    else if (X < 0 && Y > 0)
+    {
+        result = 2;
+    }
+    else if (X < 0 && Y < 0)
+    {
+        result = 3;
+    }
+    else if (X > 0 && Y < 0)
+    {
+        result = 4;
+    }
+    return result;
+}
+void GimmeGod(int X, int Y)
+{
+    int quater = GetNumberOfQuater(X, Y);
+    if (quater > 0)
+    {
+        Console.WriteLine($"Точка {X}:{Y} находится в {quater} четверти!");
+    }
+    else
+    {
+        Console.WriteLine($"Точка {X}:{Y} находится на пересечении четвертей!");
+    }
+}
+GimmeGod(1,1);
+GimmeGod(-1,1);
+GimmeGod(-1,-1);
+GimmeGod(1,-1);
+GimmeGod(0,0);
+GimmeGod(0,1);
+GimmeGod(0,1);
---
+++ task012-NumberOfQuater/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+
+Console.WriteLine("Введите номер четверти");
+int quart = Convert.ToInt32(Console.ReadLine());
+
+if (quart == 1)
+{
+    Console.WriteLine("y > 0; x > 0");
+}
+else if (quart == 2)
+{
+    Console.WriteLine("y < 0; x > 0");
+}
+else if (quart == 3)
+{
+    Console.WriteLine("y < 0; x < 0");
+}
+else if (quart == 4)
+{
+    Console.WriteLine("y > 0; x < 0");
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+}
