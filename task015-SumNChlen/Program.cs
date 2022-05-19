90d420d19dc085e63746c7ba0f7fb92b1d025147
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-19 23:01:24 +0300

    add tasks 015/016/017/018 from seminar 22-05-19

---
+++ task015-SumNChlen/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+int GetSomOfSeries(int from, int to)
+{
+    int result = 0;
+    for (int i = from; i <= to; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+int sumOrSeries = GetSomOfSeries(1,number);
+System.Console.WriteLine(sumOrSeries);
---
+++ task017-FactorialN/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+int multi = 1;
+for (int i = 1; i <= number; i++)
+{
+    multi *= i;
+}
+System.Console.WriteLine(multi);
