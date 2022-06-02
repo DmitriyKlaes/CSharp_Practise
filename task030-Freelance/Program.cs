f779c528997f29ccc0dddcb85d148e5e9c7a9e74
Dmitriy_Klaes <89119584059@mail.ru>
2022-06-02 22:20:59 +0300

    tasks 030/031/032/033 from seminar 22-06-02

---
+++ task030-Freelance/Program.cs
@@ -0,0 +1,41 @@
+﻿int[,] GenerateArray(int rowLength, int colLength, int minBorder = 0, int maxBorder = 20)
+{
+    int[,] generateArray = new int[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            generateArray[i, j] = new Random().Next(minBorder, maxBorder + 1);
+        }
+    }
+    return generateArray;
+}
+void PrintHederOfNumbers(int length, string name = "")
+{
+    if (string.IsNullOrEmpty(name))
+    {
+        System.Console.WriteLine($"---------------{name}--------------");
+    }
+    Console.WriteLine($"[ ]\t");
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"[{i}]\t");
+    }
+    Console.WriteLine();
+}
+void Print2DArray(int[,] arrayToPrint, string name = "")
+{
+    PrintHederOfNumbers(arrayToPrint.GetLength(1),name);
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayToPrint[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] generatedArray = GenerateArray(7,5);
+Print2DArray(generatedArray);
---
+++ task033-2DFrequencyWords/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 57: Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+Набор данных
+Частотный массив
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза 
+1 встречается 1 раз 
+2 встречается 1 раз 
+8 встречается 1 раз 
+9 встречается 3 раза
+1, 2, 3 
+4, 6, 1 
+2, 1, 6
+1 встречается 3 раза 
+2 встречается 2 раз 
+3 встречается 1 раз 
+4 встречается 1 раз 
+6 встречается 2 раза
+*/
+
+int[,] GetNewArray(int row, int column)
+{
+    int[,] result = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void FrequencyWords(int[,] arrayTo)
+{
+    int result = 0;
+    for (int k = 0; k < 10; k++)
+    {
+        for (int i = 0; i < arrayTo.GetLength(0); i++)            
+        {
+            for (int j = 0; j < arrayTo.GetLength(1); j++)
+            {
+
+            }
+        }
+    }
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.Write($"[X]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine("");
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
