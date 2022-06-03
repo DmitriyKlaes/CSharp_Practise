6f0b2d32ff88329a3ee86fe0b1328920955797a9
DmitriyKlaes <89119584059@mail.ru>
2022-06-03 11:35:28 +0300

    Дописал задачу 030, что не успел на семнаре

--- task030-Freelance/Program.cs
+++ task030-Freelance/Program.cs
@@ -1,5 +1,12 @@
 ﻿int[,] GenerateArray(int rowLength, int colLength, int minBorder = 0, int maxBorder = 20)
 {
+    if (minBorder > maxBorder)
+    {
+        Console.WriteLine("Границы генерации не верны минимальный больше максимального," +
+                         $"minBorder: {minBorder}, maxBorder: {maxBorder}");
+        minBorder = 0;
+        maxBorder = 20;
+    }
     int[,] generateArray = new int[rowLength, colLength];
     for (int i = 0; i < rowLength; i++)
     {
@@ -10,32 +17,57 @@
     }
     return generateArray;
 }
+
 void PrintHederOfNumbers(int length, string name = "")
 {
-    if (string.IsNullOrEmpty(name))
+    if (!string.IsNullOrEmpty(name))
     {
-        System.Console.WriteLine($"---------------{name}--------------");
+        System.Console.WriteLine($"---------------{name}---------------");
     }
-    Console.WriteLine($"[ ]\t");
+    Console.Write($"[ ]\t");
     for (int i = 0; i < length; i++)
     {
-        Console.WriteLine($"[{i}]\t");
+        Console.Write($"[{i}]\t");
     }
     Console.WriteLine();
 }
+
 void Print2DArray(int[,] arrayToPrint, string name = "")
 {
-    PrintHederOfNumbers(arrayToPrint.GetLength(1),name);
+    PrintHederOfNumbers(arrayToPrint.GetLength(1), name);
     for (int i = 0; i < arrayToPrint.GetLength(0); i++)
     {
-        Console.WriteLine();
+        Console.Write($"[{i}]\t");
         for (int j = 0; j < arrayToPrint.GetLength(1); j++)
         {
-            Console.Write($"{arrayToPrint[i,j]}\t");
+            Console.Write($"{arrayToPrint[i, j]}\t");
         }
         Console.WriteLine();
     }
 }
 
-int[,] generatedArray = GenerateArray(7,5);
-Print2DArray(generatedArray);
+void PrintArray(int[] array, int rowIndex, string name = "")
+{
+    PrintHederOfNumbers(array.Length, name);
+    Console.Write($"[{rowIndex}]\t");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+}
+
+int[] getRowFrom2DArray(int[,] twoDArray, int rowIndex)
+{
+    int[] result = new int[twoDArray.GetLength(1)];
+    for (int i = 0; i < twoDArray.GetLength(1); i++)
+    {
+        result[i] = twoDArray[rowIndex, i];
+    }
+    return result;
+}
+
+int[,] generatedArray = GenerateArray(7, 5, maxBorder: 100, minBorder: -20);
+Print2DArray(generatedArray, "Двухмерный массив");
+const int rowIndex = 2;
+int[] oneRow = getRowFrom2DArray(generatedArray, rowIndex);
+PrintArray(oneRow, rowIndex, $"Строка двухмерного массива №{rowIndex}");
