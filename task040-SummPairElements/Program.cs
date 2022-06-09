76b3f72b011aec04fb63eca97dfa99888a050793
Dmitriy_Klaes <89119584059@mail.ru>
2022-06-10 00:27:06 +0300

    tasks 040/041/042 from seminar 22-06-10

---
+++ task040-SummPairElements/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 70: Напишите программу, 
+которая на вход принимает два числа и выдаёт первые N чисел, 
+для которых каждое следующее равно сумме двух предыдущих.
+3 и 4, N = 5 -> 3 4 7 11 18
+6 и 10, N = 4 -> 6 10 16 26 
+*/
+
+void SummPairElements(int firstNumber, int secondNumber, int count)
+{
+    if (count == 0)
+    {
+        return;
+    }
+    Console.Write($"{firstNumber} ");
+    SummPairElements(secondNumber, firstNumber + secondNumber, count - 1);
+}
+SummPairElements(3,4,5);
---
+++ task042-TwoArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 72: Заданы 2 массива: info и data. 
+В массиве info хранятся двоичные представления нескольких чисел (без разделителя). 
+В массиве data хранится информация о количестве бит, которые занимают числа из массива info. 
+Напишите программу, которая составит массив десятичных представлений 
+чисел массива data с учётом информации из массива info.
+
+входные данные:
+info = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+data = {2, 3, 3, 1 } // {2, 3, 2, 2 }
+выходные данные:
+1, 7, 0, 1
+*/
+
+int ToDicimal (int[] array, int number, int count = 0)
+{
+    int[] result = new int[number];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = array[i];
+    }
+    
+}
+
+int[] DecimialNumbersArray(int[] arrayInfo, int[] arrayData)
+{
+    int[] result = new int[arrayData.Length];
+
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = 
+    }
+
+    return result;
+}
+
