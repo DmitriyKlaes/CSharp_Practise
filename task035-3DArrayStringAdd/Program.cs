6d577c4759b59c7bde5ef6c585af51139973f126
Dmitriy_Klaes <89119584059@mail.ru>
2022-06-14 23:14:41 +0300

    дописал задачу 035 с трезмерным массивом

--- task035-3DArrayStringAdd/Program.cs
+++ task035-3DArrayStringAdd/Program.cs
@@ -17,7 +17,12 @@ int GetUniqNumber()
         }
         else
         {
-            int countUniq = GetUniqNumber(numbers);
+            int countUniq = GetNumberOfUniq(numbers);
+            if (countUniq == 90)
+            {
+                Console.WriteLine("Все двузначные числа использованы");
+                break;
+            }
         }
     }
     return result;
@@ -26,7 +31,14 @@ int GetUniqNumber()
 int GetNumberOfUniq(int[] numbers)
 {
     int result = new int();
-
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        if (numbers[i] == 1)
+        {
+            result++;
+        }
+    }
+    return result;
 }
 
 int[,,] Generate3DArray(int rowsNumber, int colsNumber, int depthNumber)
@@ -54,14 +66,13 @@ void print3DArray(int[,,] arrayToPrint, int oneLineLength = 3)
         {
             for (int z = 0; z < arrayToPrint.GetLongLength(2); z++)
             {
-                Console.Write($"[{i},{j},{z}:{arrayToPrint[i, j, z]}]\t");
+                Console.Write($"[{i},{j},{z}: {arrayToPrint[i, j, z]}]\t");
                 if (currentLength == oneLineLength)
                 {
                     Console.WriteLine();
                     currentLength = 0;
                 }
                 currentLength++;
-
             }
         }
     }
