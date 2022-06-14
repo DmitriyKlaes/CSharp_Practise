9a51b4315330a859dc9973cd5a014f382cd7c82a
DmitriyKlaes <89119584059@mail.ru>
2022-06-14 17:40:18 +0300

    мучаю 042, еще не продумал алгоритм

--- task042-TwoArrays/Program.cs
+++ task042-TwoArrays/Program.cs
@@ -6,31 +6,58 @@
 чисел массива data с учётом информации из массива info.
 
 входные данные:
-info = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
-data = {2, 3, 3, 1 } // {2, 3, 2, 2 }
+info = { 0, 1, 1, 1, 1, 0, 0, 0, 1 }
+data = { 2, 3, 3, 1 }
 выходные данные:
 1, 7, 0, 1
 */
 
-int ToDicimal (int[] array, int number, int count = 0)
+int[] DecimialNumbersArray(int[] arrayInfo, int[] arrayData)
+{
+    int[] result = new int[arrayData.Length];
+    int index = 0;
+    for (int i = 0; i < arrayData.Length; i++)
+    {
+        for (int j = arrayData[i] - 1; j >= 0; j--, index++)        
+        {
+            int temp = index * Convert.ToInt32(Math.Pow(2, 3));
+            result[i] = arrayInfo[j] * 
+        }
+    }
+
+    return result;
+}
+
+int[] info = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] data = { 2, 3, 3, 1 };
+
+
+
+
+
+
+int ToDicimal(int[] array, int number, int count = 0)
 {
     int[] result = new int[number];
     for (int i = 0; i < result.Length; i++)
     {
         result[i] = array[i];
     }
-    
+
 }
 
-int[] DecimialNumbersArray(int[] arrayInfo, int[] arrayData)
-{
-    int[] result = new int[arrayData.Length];
 
-    for (int i = 0; i < length; i++)
-    {
-        result[i] = 
-    }
 
-    return result;
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
