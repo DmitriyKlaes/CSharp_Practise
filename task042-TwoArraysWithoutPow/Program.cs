d28d7a9a0cb2f25d976879827dceacc0c00204c8
DmitriyKlaes <89119584059@mail.ru>
2022-06-15 16:20:01 +0300

    сделал задачу 042 без Math.Pow

--- task042-TwoArraysWithoutPow/Program.cs
+++ task042-TwoArraysWithoutPow/Program.cs
@@ -17,18 +17,14 @@ int[] DecimialNumbersArray(int[] arrayInfo, int[] arrayData)
     int[] result = new int[arrayData.Length];
     int index = 0;
     int basePowerIndex = 1;
-    for (int i = 0; i < arrayData.Length; i++)
+    for (int i = 0; i < result.Length; i++)
     {
         for (int j = 0; j < arrayData[i]; j++, index++)
         {
-            for (int k = 0; k < j; k++)
+            for (int k = j; k < arrayData[i] - 1; k++)
             {
                 basePowerIndex = basePowerIndex * 2;
             }
-            //if (i == 0)
-            //{
-            //    basePowerIndex = 1;
-            //}
             result[i] = result[i] + arrayInfo[index] * basePowerIndex;
             basePowerIndex = 1;
         }
@@ -36,8 +32,8 @@ int[] DecimialNumbersArray(int[] arrayInfo, int[] arrayData)
     return result;
 }
 
-int[] info = { 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1 };
-int[] data = { 3, 2, 3, 3, 1 };
+int[] info = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] data = { 2, 3, 3, 1 };
 int[] resultArray = DecimialNumbersArray(info, data);
 string stringResultArray = string.Join(", ", resultArray);
 System.Console.WriteLine(stringResultArray);
